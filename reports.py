63082e9f87b6d6ff485682e8bda42867db19ef59
Maretzky85 <34583771+Maretzky85@users.noreply.github.com>
2018-02-11 09:36:44 +0100

    delete unnesesary comments

--- reports.py
+++ reports.py
@@ -5,7 +5,6 @@ def open_file(file_name="game_stat.txt"):
     try:
         with open(file_name, 'r', encoding='utf-8') as f:
             content = f.readlines()
-            #print(content)
     except FileNotFoundError:
         print("File not found")
     except OSError:
@@ -16,30 +15,23 @@ def parse_content(file_name="game_stat.txt"):
     '''Takes content table(by line), transforms every line to dict and returns table of dict'''
     data_list=[]
     content=open_file(file_name)
-    #print(content)
     for entry in content:
-        #print(entry)
         entry = entry.strip().split("\t")
-        #print(entry)
         try:
-            #print("value = " +value)
             dict_entry={}
             dict_entry["title"]=entry[0]
             dict_entry["copies"]=entry[1]
             dict_entry["release"]=entry[2]
             dict_entry["genre"]=entry[3]
             dict_entry["publisher"]=entry[4]
-            #print(dict_entry)
             data_list.append(dict_entry)
         except IndexError:
             print("Wrong format")
         except ValueError:
             print("Wrong format")
-        #print(data_list)
     return(data_list)
 
 def count_games(file_name="game_stat.txt"):
-    #print (content)
     content=parse_content(file_name)
     val = 0
     for i in content:
@@ -101,7 +93,6 @@ def get_genres(file_name="game_stat.txt"):
 def sort_table(content_table):
     for entry in content_table:
         for i in range(0, len(content_table)-1):
-            #print("{} {}".format(name_table[i+1][0], name_table[i][0]))
             if content_table[i+1] < content_table[i]:
                 temp = content_table[i]
                 content_table[i]=content_table[i+1]
@@ -118,18 +109,3 @@ def when_was_top_sold_fps(file_name="game_stat.txt"):
             top_entry_nr=entry_nr
         entry_nr+=1
     return int(content[top_entry_nr]["release"])
-
-
-#def test():
-    #parse_content()
-    #print(count_games())
-    #print(open_file())
-    #print(parse_content())
-    #print(decide("game_stat.txt",2012))
-    #print(get_latest("game_stat.txt"))
-    #print(count_by_genre("game_stat.txt", "First-person shooter"))
-    #print(get_line_number_by_title("game_stat.txt", "Diablo III"))
-    #print(sort_abc("game_stat.txt"))
-    #print(get_genres("game_stat.txt"))
-    #print(when_was_top_sold_fps("game_stat.txt"))
-#test()
